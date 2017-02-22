dee0c397f21234de94ee8400eda86deb5e00bd4b
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-21 19:14:18 -0500

    Update interview_questions.js

--- interview_questions.js
+++ interview_questions.js
@@ -67,8 +67,10 @@ function fib(n) {
 	};
 
 	if (n === 0) {
-		return [0];
+		return [];
 	} else if (n === 1) {
+		return [0];
+	} else if (n === 2) {
 		return numArray;
 	} else return numArray;
 };
